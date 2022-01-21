a57353fffcfd7850d55880751520482ee37e3593
snowleopardAlexa <alexaslomski@gmail.com>
2022-01-20 21:16:44 -0500

    add more photos

--- src/data/data.js
+++ src/data/data.js
@@ -3,6 +3,10 @@ import badger from '../img/badger.jpg';
 import snake from '../img/snake.jpg';
 import eel from '../img/eel.jpg';
 import crocodile from '../img/crocodile.jpg';
+import toad from '../img/toad.jpg';
+import frog from '../img/frog.jpg';
+import salamander from '../img/salamander.jpg';
+import tortoise from '../img/tortoise.jpg';
 
 const animals = [
     {
@@ -43,6 +47,38 @@ const animals = [
         category: 'reptile',
         status: 'safe',
         img: crocodile,
+        desc: `Flamingos are fabulous birds that are very social...`
+    },
+    {
+        id: 6,
+        title: 'Toad',
+        category: 'amphibian',
+        status: 'safe',
+        img: toad,
+        desc: `Flamingos are fabulous birds that are very social...`
+    },
+    {
+        id: 7,
+        title: 'Frog',
+        category: 'amphibian',
+        status: 'safe',
+        img: frog,
+        desc: `Flamingos are fabulous birds that are very social...`
+    },
+    {
+        id: 8,
+        title: 'Tortoise',
+        category: 'reptile',
+        status: 'safe',
+        img: tortoise,
+        desc: `Flamingos are fabulous birds that are very social...`
+    },
+    {
+        id: 9,
+        title: 'Salamander',
+        category: 'amphibian',
+        status: 'safe',
+        img: salamander,
         desc: `Flamingos are fabulous birds that are very social...`
     },
 ]
