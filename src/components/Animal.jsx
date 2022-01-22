ea1689561b69baa0e05acfbb1ecc402424d33309
snowleopardAlexa <alexaslomski@gmail.com>
2022-01-21 20:34:51 -0500

    add status and animal info

--- src/components/Animal.jsx
+++ src/components/Animal.jsx
@@ -5,10 +5,16 @@ const Animal = () => {
   return (
     <div className="section-center">
       {animals.map((animalSpecies) => {
-        const { id, title, img, desc } = animalSpecies;
+        const { id, title, img, status, desc } = animalSpecies;
         return (
           <article key={id} className="menu-item">
             <img src={img} alt={title} className="photo" />
+            <div className="animal-info">
+              <header>
+                <h4>{title}</h4>
+                <h4 className="status">{status}</h4>
+              </header>
+            </div>
           </article>
         );
       })}
