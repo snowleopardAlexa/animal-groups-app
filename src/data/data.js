0f80f9ddcfc1109c745e8cf9c11a88b3d2a3b5df
snowleopardAlexa <alexaslomski@gmail.com>
2022-01-23 20:05:27 -0500

    create data.js

---
+++ src/data/data.js
@@ -0,0 +1,20 @@
+const animals = [
+    {
+        id: 1,
+        title: 'Flamingo',
+        category: 'bird',
+        status: 'safe',
+        img: './img/flamingo.jpg',
+        desc: `Flamingos are fabulous birds that are very social...`
+    },
+    {
+        id: 1,
+        title: 'Badger',
+        category: 'mammal',
+        status: 'safe',
+        img: './img/badger.jpg',
+        desc: `Flamingos are fabulous birds that are very social...`
+    }
+]
+
+export default animals;
